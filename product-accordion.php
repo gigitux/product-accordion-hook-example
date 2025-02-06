1bb929ad3fab932fafc7ece3742f795eb036ea20
Luigi Teschio <gigitux@gmail.com>
2025-02-06 17:12:14 +0100

    feat: add block hooks second approach

--- product-accordion.php
+++ product-accordion.php
@@ -72,43 +72,31 @@ add_filter(
 	4
 );
 
-// add_filter(
-// 	'hooked_block_types',
-// 	function ( $hooked_block_types, $relative_position, $anchor_block_type, $context ) {
+add_filter(
+	'hooked_block_sample/product-accordion',
+	function ( $parsed_hooked_block, $hooked_block_type, $relative_position, $parsed_anchor_block, $context) {
+		$parsed_block_from_mockup = '<!-- wp:sample/product-accordion -->
+<div class="wp-block-sample-product-accordion"><!-- wp:woocommerce/accordion-item {"openByDefault":true} -->
+<div class="is-open wp-block-woocommerce-accordion-item"><!-- wp:woocommerce/accordion-header -->
+<h3 class="wp-block-woocommerce-accordion-header accordion-item__heading"><button class="accordion-item__toggle"><span>Test</span><span class="accordion-item__toggle-icon has-icon-plus" style="width:1.2em;height:1.2em"><svg width="1.2em" height="1.2em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path d="M11 12.5V17.5H12.5V12.5H17.5V11H12.5V6H11V11H6V12.5H11Z" fill="currentColor"></path></svg></span></button></h3>
+<!-- /wp:woocommerce/accordion-header -->
+<!-- wp:woocommerce/accordion-panel -->
+<div class="wp-block-woocommerce-accordion-panel"><div class="accordion-content__wrapper"><!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget turpis eget nunc fermentum ultricies. Nullam nec sapien nec0</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:woocommerce/accordion-panel --></div>
+<!-- /wp:woocommerce/accordion-item --></div>
+<!-- /wp:sample/product-accordion --></div>';
+
+		$parsed_block_from_mockup = parse_blocks( $parsed_block_from_mockup );
 
 
 
-// 		if ( 'core/post-content' === $anchor_block_type && 'after' === $relative_position ) {
-// 			$hooked_block_types[] = 'core/paragraph';
+		return $parsed_block_from_mockup[0];
 
-// 		}
+	},
+	10,
+	5
+);
 
-// 		return $hooked_block_types;
-// 	},
-// 	10,
-// 	4
-// );
 
-// function modify_hooked_copyright_date_block_in_footer( $parsed_hooked_block, $hooked_block_type, $relative_position, $parsed_anchor_block, $context  ) {
-
-// 	// Has the hooked block been suppressed by a previous filter?
-// 	if ( is_null( $parsed_hooked_block ) ) {
-// 		return $parsed_hooked_block;
-// 	}
-
-// 	// Only apply the updated attributes if the block is hooked after a Site Title block.
-// 	if (
-// 		'core/post-content' === $parsed_anchor_block['blockName'] && 'after' === $relative_position
-// 	) {
-// 		$parsed_hooked_block['attrs'] = array(
-// 			'content'     => '2019',
-// 		);
-
-// 		$parsed_hooked_block['innerContent'] = array(
-// 			'<p><a href="#">' . __( 'Back to top' ) . '</a></p>'
-// 		);
-// 	}
-
-// 	return $parsed_hooked_block;
-// }
-// add_filter( 'hooked_block_core/paragraph', 'modify_hooked_copyright_date_block_in_footer', 10, 5 );
